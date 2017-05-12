7b9cd3352e3dfdef242c29a30de8fb2d7f3fc253
Skylar Moore <nickgray0@gmail.com>
2017-05-11 20:57:44 -0700

    more of lexical_cast

--- Lib/lexical_cast.hpp
+++ Lib/lexical_cast.hpp
@@ -77,3 +77,75 @@ template<>
 auto lexical_cast<long double>(const StandardTemplateLibrary::WString &Object) {
 	return std::wcstold(Object.Data(), nullptr);
 }
+
+template<>
+auto lexical_cast<int>(const StandardTemplateLibrary::String &Object) {
+	auto Value = lexical_cast<long>(Object);
+	return static_cast<int>(Value);
+}
+
+template<>
+auto lexical_cast<int>(const StandardTemplateLibrary::WString &Object) {
+	auto Value = lexical_cast<long>(Object);
+	return static_cast<int>(Value);
+}
+
+template<>
+auto lexical_cast<short>(const StandardTemplateLibrary::String &Object) {
+	auto Value = lexical_cast<long>(Object);
+	return static_cast<short>(Value);
+}
+
+template<>
+auto lexical_cast<short>(const StandardTemplateLibrary::WString &Object) {
+	auto Value = lexical_cast<long>(Object);
+	return static_cast<short>(Value);
+}
+
+template<>
+auto lexical_cast<signed char>(const StandardTemplateLibrary::String &Object) {
+	auto Value = lexical_cast<long>(Object);
+	return static_cast<signed char>(Value);
+}
+
+template<>
+auto lexical_cast<signed char>(const StandardTemplateLibrary::WString &Object) {
+	auto Value = lexical_cast<long>(Object);
+	return static_cast<signed char>(Value);
+}
+
+template<>
+auto lexical_cast<unsigned int>(const StandardTemplateLibrary::String &Object) {
+	auto Value = lexical_cast<unsigned long>(Object);
+	return static_cast<unsigned int>(Value);
+}
+
+template<>
+auto lexical_cast<unsigned int>(const StandardTemplateLibrary::WString &Object) {
+	auto Value = lexical_cast<unsigned long>(Object);
+	return static_cast<unsigned int>(Value);
+}
+
+template<>
+auto lexical_cast<unsigned short>(const StandardTemplateLibrary::String &Object) {
+	auto Value = lexical_cast<unsigned long>(Object);
+	return static_cast<unsigned short>(Value);
+}
+
+template<>
+auto lexical_cast<unsigned short>(const StandardTemplateLibrary::WString &Object) {
+	auto Value = lexical_cast<unsigned long>(Object);
+	return static_cast<unsigned short>(Value);
+}
+
+template<>
+auto lexical_cast<unsigned char>(const StandardTemplateLibrary::String &Object) {
+	auto Value = lexical_cast<unsigned long>(Object);
+	return static_cast<unsigned char>(Value);
+}
+
+template<>
+auto lexical_cast<unsigned char>(const StandardTemplateLibrary::WString &Object) {
+	auto Value = lexical_cast<unsigned long>(Object);
+	return static_cast<unsigned char>(Value);
+}
